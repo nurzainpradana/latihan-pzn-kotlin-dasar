9bf5523a6a88b88d833257c05d025857bf98c82b
nurzainpradana <pradananurzain@gmail.com>
2020-10-25 03:16:10 +0700

    materi 41. komentar

---
+++ src/main/kotlin/Komentar.kt
@@ -0,0 +1,28 @@
+/**
+ * Function untuk contoh
+ */
+fun contohFunction(){
+
+}
+
+/**
+ * Contoh function dengan parameter
+ * @param name nama parameter
+ */
+fun contohFunctionParameter(name: String){
+
+}
+
+fun main() {
+    /*
+
+    // memanggil contohFunction()
+    contohFunction()
+    contohFunctionParameter() // memanggil contohFunctionParameter
+
+     */
+
+    /**
+     * Komentar seperti java doc
+     */
+}
