60e2b332da743041b9179309e74c56e2ca793e4f
nurzainpradana <pradananurzain@gmail.com>
2020-10-25 03:04:24 +0700

    materi 39. package & import

---
+++ src/main/kotlin/Package.kt
@@ -0,0 +1,7 @@
+import com.company.belajar.util.sayGoodbye
+import com.company.belajar.util.sayHello
+
+fun main() {
+    sayHello("Zain")
+    sayGoodbye("Pradana")
+}
--- src/main/kotlin/com/company/belajar/util/StringUtil.kt
+++ src/main/kotlin/com/company/belajar/util/StringUtil.kt
@@ -1,2 +1,10 @@
 package com.company.belajar.util
 
+fun sayHello(name: String){
+    println("Hello $name")
+}
+
+fun sayGoodbye(name: String){
+    println("Say $name")
+}
+
