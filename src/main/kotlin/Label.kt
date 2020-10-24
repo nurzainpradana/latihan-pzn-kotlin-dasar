6e3ed31aea134810e585b7dd413e02e3647b02e1
nurzainpradana <pradananurzain@gmail.com>
2020-10-25 02:43:30 +0700

    materi 38. label

---
+++ src/main/kotlin/Label.kt
@@ -0,0 +1,41 @@
+fun main() {
+    fun labelBreak(){
+        loopI@ for (i in 1..10){
+            loopJ@ for (j in 1..10){
+                if (i > 5){
+                    //break
+                    break@loopI
+                }
+
+                if (j == 5){
+                    continue@loopI
+                }
+                println("$i * $j = ${i * j}")
+            }
+        }
+    }
+
+    fun labelContinue(){
+        loopI@ for (i in 1..10){
+            loopJ@ for (j in 1..10){
+                if (j == 5){
+                    continue@loopI
+                }
+
+                println("$i * $j = ${i * j}")
+            }
+        }
+    }
+
+    fun test(name: String, operation: (String) -> Unit): Unit = operation(name)
+
+    test("") test@{
+        if (it == "") {
+            return@test
+        } else {
+            println("Zain")
+        }
+    }
+    //labelBreak()
+    //labelContinue()
+}
